45519ce0c8e8f41c7a3efd9bd5ffa83ea9959645
subhechhu <subhechhu.khanal@gmail.com>
2020-12-16 20:19:46 +0545

    app icon changed

--- app/src/main/java/com/subhechhu/bhadama/LoginActivity.java
+++ app/src/main/java/com/subhechhu/bhadama/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.subhechhu.bhadama;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,56 +16,149 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class LoginActivity extends AppCompatActivity {
     EditText editText_phone, editText_pin;
+    AppCompatButton button_login_phone_verify, button_login_pin_verify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
         editText_phone = findViewById(R.id.edittext_login_phone);
         editText_pin = findViewById(R.id.edittext_login_pin);
+
+        button_login_phone_verify = findViewById(R.id.button_login_phone_verify);
+        button_login_pin_verify = findViewById(R.id.button_login_pin_verify);
+
+        editText_phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                if (arg0.toString().length() == 10)
+                    button_login_phone_verify.setVisibility(View.INVISIBLE);
+                else
+                    button_login_phone_verify.setVisibility(View.VISIBLE);
+            }
+        });
+
+        editText_pin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                if (arg0.toString().length() == 4)
+                    button_login_pin_verify.setVisibility(View.INVISIBLE);
+                else
+                    button_login_pin_verify.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     public void signUp(View v) {
         startActivity(new Intent(LoginActivity.this, SignupActivity.class));
     }
 
-    public void forgotPin(View v){
+    public void forgotPin(View v) {
 
-        View view = getLayoutInflater().inflate(R.layout.otp_dialog, null);
-        BottomSheetDialog dialog = new BottomSheetDialog(LoginActivity.this);
-        dialog.setContentView(view);
-        dialog.setCancelable(false);
-        dialog.show();
+        if (editText_phone.getText().toString().length() != 10) {
+            Toast.makeText(LoginActivity.this, "Please enter phone number to proceed", Toast.LENGTH_LONG).show();
+        } else {
+            View view = getLayoutInflater().inflate(R.layout.otp_dialog, null);
+            BottomSheetDialog dialog = new BottomSheetDialog(LoginActivity.this, R.style.dialogStyle);
+            dialog.setContentView(view);
+            dialog.setCancelable(false);
+            dialog.show();
 
-        EditText editText_otp = view.findViewById(R.id.edittext_otp_otp);
+            EditText editText_otp = view.findViewById(R.id.edittext_otp_otp);
 
-        view.findViewById(R.id.button_otp_close).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-        view.findViewById(R.id.button_otp_verify).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(LoginActivity.this,"OTP entered is "+editText_otp.getText().toString(),Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-                renderNewPin();
-            }
-        });
+            view.findViewById(R.id.button_otp_close).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+            view.findViewById(R.id.button_otp_verify).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(LoginActivity.this, "OTP entered is " + editText_otp.getText().toString(), Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    renderNewPin();
+                }
+            });
+        }
     }
 
     private void renderNewPin() {
         View view = getLayoutInflater().inflate(R.layout.newpin_dialog, null);
-        BottomSheetDialog dialog = new BottomSheetDialog(LoginActivity.this);
+        BottomSheetDialog dialog = new BottomSheetDialog(LoginActivity.this, R.style.dialogStyle);
         dialog.setContentView(view);
         dialog.setCancelable(false);
         dialog.show();
 
         EditText editText_pin = view.findViewById(R.id.edittext_newpin_pin);
-        EditText edit_confirmpin = view.findViewById(R.id.edittext_newpin_confirmpin);
+        EditText edit_confirmpin = view.findViewById(R.id.edittext_newpin_reenterpin);
+
+        AppCompatButton button_newpin_pin_verify = view.findViewById(R.id.button_newpin_pin_verify);
+        AppCompatButton button_newpin_reenterpin_verify = view.findViewById(R.id.button_newpin_reenterpin_verify);
+
+
+        editText_pin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (verifyPin(editText_pin, edit_confirmpin, button_newpin_reenterpin_verify)) {
+                    button_newpin_pin_verify.setVisibility(View.INVISIBLE);
+                } else {
+                    button_newpin_pin_verify.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        edit_confirmpin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (verifyReenterPin(edit_confirmpin, editText_pin)) {
+                    button_newpin_reenterpin_verify.setVisibility(View.INVISIBLE);
+                } else {
+                    button_newpin_reenterpin_verify.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
 
         view.findViewById(R.id.button_newpin_close).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,9 +169,32 @@ public class LoginActivity extends AppCompatActivity {
         view.findViewById(R.id.button_newpin_proceed).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(LoginActivity.this,"New pin has been generated.\nProceed to login",Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
+                proceed(dialog, editText_pin, edit_confirmpin, button_newpin_reenterpin_verify);
             }
         });
+    }
+
+    private void proceed(BottomSheetDialog dialog, EditText edittext_Login_Reenterpin, EditText edittext_login_pin, AppCompatButton button_newpin_reenterpin_verify) {
+        if (verifyPin(edittext_login_pin, edittext_Login_Reenterpin, button_newpin_reenterpin_verify ) &&
+                verifyReenterPin(edittext_Login_Reenterpin, edittext_login_pin)) {
+            Toast.makeText(LoginActivity.this, "New pin has been generated.\nProceed to login", Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
+        }
+    }
+
+    private boolean verifyReenterPin(EditText edittext_login_reenterpin, EditText edittext_login_pin) {
+        if (edittext_login_reenterpin.getText().toString().length() == 4 && !edittext_login_reenterpin.getText().toString().equals(edittext_login_pin.getText().toString())) {
+            Toast.makeText(LoginActivity.this, "Pin did not match", Toast.LENGTH_LONG).show();
+        }
+        return edittext_login_reenterpin.getText().toString().equals(edittext_login_pin.getText().toString());
+    }
+
+    private boolean verifyPin(EditText edittext_login_pin, EditText edittextSignupReenterpin, AppCompatButton button_signup_reenterpin_verify) {
+        if(edittext_login_pin.getText().toString().length() == 4 && edittext_login_pin.getText().toString().equals(edittextSignupReenterpin.getText().toString())){
+            button_signup_reenterpin_verify.setVisibility(View.INVISIBLE);
+        }else {
+            button_signup_reenterpin_verify.setVisibility(View.VISIBLE);
+        }
+        return edittext_login_pin.getText().toString().length() == 4;
     }
 }
--- app/src/main/java/com/subhechhu/bhadama/SignupActivity.java
+++ app/src/main/java/com/subhechhu/bhadama/SignupActivity.java
@@ -1,8 +1,12 @@
 package com.subhechhu.bhadama;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,21 +15,161 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class SignupActivity extends AppCompatActivity {
 
+    final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        EditText edittext_signup_name, edittext_signup_phone, edittext_signup_pin, edittext_signup_reenterpin, edittext_signup_email;
+        AppCompatButton button_signup_phone_verify, button_signup_pin_verify, button_signup_reenterpin_verify, button_signup_email_verify, button_signup_proceed;
+
+        edittext_signup_email = findViewById(R.id.edittext_signup_email);
+        edittext_signup_reenterpin = findViewById(R.id.edittext_signup_reenterpin);
+        edittext_signup_pin = findViewById(R.id.edittext_signup_pin);
+        edittext_signup_phone = findViewById(R.id.edittext_signup_phone);
+        edittext_signup_name = findViewById(R.id.edittext_signup_name);
+
+        button_signup_email_verify = findViewById(R.id.button_signup_email_verify);
+        button_signup_reenterpin_verify = findViewById(R.id.button_signup_reenterpin_verify);
+        button_signup_pin_verify = findViewById(R.id.button_signup_pin_verify);
+        button_signup_phone_verify = findViewById(R.id.button_signup_phone_verify);
+        button_signup_proceed = findViewById(R.id.button_signup_proceed);
+
+        edittext_signup_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (verifyEmail(edittext_signup_email)) {
+                    button_signup_email_verify.setVisibility(View.INVISIBLE);
+                } else {
+                    button_signup_email_verify.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        edittext_signup_reenterpin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (verifyReenterPin(edittext_signup_reenterpin, edittext_signup_pin)) {
+                    button_signup_reenterpin_verify.setVisibility(View.INVISIBLE);
+                } else {
+                    button_signup_reenterpin_verify.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        edittext_signup_pin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (verifyPin(edittext_signup_pin, edittext_signup_reenterpin, button_signup_reenterpin_verify )) {
+                    button_signup_pin_verify.setVisibility(View.INVISIBLE);
+                } else {
+                    button_signup_pin_verify.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        edittext_signup_phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (verifyphone(edittext_signup_phone)) {
+                    button_signup_phone_verify.setVisibility(View.INVISIBLE);
+                } else {
+                    button_signup_phone_verify.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        button_signup_proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!edittext_signup_name.getText().toString().isEmpty() &&
+                        verifyEmail(edittext_signup_email) &&
+                        verifyReenterPin(edittext_signup_reenterpin, edittext_signup_pin) &&
+                        verifyPin(edittext_signup_pin, edittext_signup_reenterpin, button_signup_reenterpin_verify ) &&
+                        verifyphone(edittext_signup_phone)) {
+                    proceedSignup();
+                } else {
+                    Toast.makeText(SignupActivity.this, "All fields must be entered", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    public void proceedSignup(View v){
+    private boolean verifyEmail(EditText edittext_signup_email) {
+        return edittext_signup_email.getText().toString().trim().matches(emailPattern);
+    }
+
+    private boolean verifyReenterPin(EditText edittextSignupReenterpin, EditText edittext_signup_pin) {
+        if (edittextSignupReenterpin.getText().toString().length() == 4 && !edittextSignupReenterpin.getText().toString().equals(edittext_signup_pin.getText().toString())) {
+            Toast.makeText(SignupActivity.this, "Pin did not match", Toast.LENGTH_LONG).show();
+        }
+        return edittextSignupReenterpin.getText().toString().equals(edittext_signup_pin.getText().toString());
+    }
+
+    private boolean verifyPin(EditText edittext_signup_pin, EditText edittextSignupReenterpin, AppCompatButton button_signup_reenterpin_verify) {
+        if(edittext_signup_pin.getText().toString().length() == 4 && edittext_signup_pin.getText().toString().equals(edittextSignupReenterpin.getText().toString())){
+            button_signup_reenterpin_verify.setVisibility(View.INVISIBLE);
+        }else {
+            button_signup_reenterpin_verify.setVisibility(View.VISIBLE);
+        }
+        return edittext_signup_pin.getText().toString().length() == 4;
+    }
+
+    private boolean verifyphone(EditText edittext_signup_phone) {
+        return edittext_signup_phone.getText().toString().length() == 10;
+    }
+
+
+    public void proceedSignup() {
         View view = getLayoutInflater().inflate(R.layout.otp_dialog, null);
-        BottomSheetDialog dialog = new BottomSheetDialog(SignupActivity.this);
+        BottomSheetDialog dialog = new BottomSheetDialog(SignupActivity.this,  R.style.dialogStyle);
         dialog.setContentView(view);
         dialog.setCancelable(false);
         dialog.show();
 
         EditText editText_otp = view.findViewById(R.id.edittext_otp_otp);
-
         view.findViewById(R.id.button_otp_close).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +180,7 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                Toast.makeText(SignupActivity.this,"OTP entered is "+editText_otp.getText().toString(),Toast.LENGTH_SHORT).show();
-                Toast.makeText(SignupActivity.this,"Registration Successful",Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignupActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
                 finish();
             }
